8dbb15816aaba3c0c31edef5d048e3a843531061
mohamedaziz14 <jo56501283@gmail.com>
2024-02-22 14:37:32 +0100

    ..

--- front-end/myApp/App.js
+++ front-end/myApp/App.js
@@ -13,7 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFDAFB',
+  
     alignItems: 'center',
     justifyContent: 'center',
   },
