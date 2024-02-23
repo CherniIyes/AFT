ec786e657c0b46ca9210557a4cf0c3178b72f991
Ghada000 <assidighada4@gmail.com>
2024-02-23 11:52:07 +0100

    modificationsss

--- back-end/server/index.js
+++ back-end/server/index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const app = express();
 const cors = require("cors");
-
+const PORT = 6464;
 const milkRoutes = require('../Routes/milkRoutes.js')
 const expensesRoutes = require('../Routes/expensesRoutes.js')
 const SalesRoutes = require('../Routes/SalesRoutes.js')
-const PORT = 6464;
 const cowsRouter = require ('../Routes/cowsRoutes')
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/cows', cowsRouter);
+
 
 app.get('/', (req, res) => {
   res.send('Server Listening');
@@ -22,7 +21,7 @@ app.use("/sales", SalesRoutes)
 app.use('/milk', milkRoutes);
 app.use("/exp",expensesRoutes)
 
-
+app.use('/cows', cowsRouter);
 
 app.get('/', (req, res) => {
       res.send('Server Listening');
