4b6bea4d00855fd043c4722591f32b066cb61ac9
CheniIyes <cheniiyes@gmail.com>
2024-02-23 11:54:09 +0100

    set

--- back-end/server/index.js
+++ back-end/server/index.js
@@ -9,18 +9,10 @@ const PORT = 6464;
 
 app.use(cors())
 app.use(express.json())
-<<<<<<< HEAD
-
-app.use('/milk', milkRoutes);
-app.use("/exp",expensesRoutes)
-
-
-=======
 app.use("/sales", SalesRoutes)
 app.use('/milk', milkRoutes);
 app.use("/exp",expensesRoutes)
 
->>>>>>> 54041af69af13847c3fde264a32dd7e8677e221a
 
 
 app.get('/', (req, res) => {
