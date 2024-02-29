7a32fd2136124085327200ce5e81d04a23174e9b
BadisBoughanmi <badisboughanmi555@gmail.com>
2024-02-29 10:00:07 +0100

    fixing the post method

--- front-end/myApp/screens/Milk.js
+++ front-end/myApp/screens/Milk.js
@@ -1,47 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, FlatList, Alert } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import axios from 'axios';
-import { Card } from 'react-native-paper'; // Import Card component from react-native-paper
-import { Ionicons } from '@expo/vector-icons'; // Import Ionicons for icons
+import { Card } from 'react-native-paper';
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 const ProfitCalculatorScreen = () => {
-  const [date, setDate] = useState(new Date().toISOString().slice(0, 10));
+  const [date, setDate] = useState("");
   const [price, setPrice] = useState('');
   const [quantity, setQuantity] = useState('');
   const [dailyProfit, setDailyProfit] = useState(0);
   const [monthlyProfit, setMonthlyProfit] = useState(0);
   const [yearlyProfit, setYearlyProfit] = useState(0);
   const [tableData, setTableData] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.get('http://192.168.1.9:6464/milk');
-      setTableData(response.data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-
-  const calculateProfit = () => {
-    const dailyProfit = parseFloat(price) * parseInt(quantity);
-    setDailyProfit(dailyProfit);
-
-    const monthlyProfit = dailyProfit * 30; // Assuming 30 days in a month
-    setMonthlyProfit(monthlyProfit);
-
-    const yearlyProfit = dailyProfit * 365; // Assuming 365 days in a year
-    setYearlyProfit(yearlyProfit);
-  };
 
   const handleSubmit = async () => {
     try {
-      const response = await axios.post('http://192.168.1.9/milk/add', {
+      const response = await axios.post('http://192.168.50.79:6464/milk/add', {
         day: date,
         price: price,
         quantity: quantity
@@ -58,14 +40,40 @@ const ProfitCalculatorScreen = () => {
     }
   };
 
+
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://192.168.50.79:6464/milk');
+      setTableData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error.message);
+      
+      setError('Error fetching data: ' + error.message);
+    }
+  };
+
+  const calculateProfit = () => {
+    const dailyProfit = parseFloat(price) * parseInt(quantity);
+    setDailyProfit(dailyProfit);
+
+    const monthlyProfit = dailyProfit * 30; // Assuming 30 days in a month
+    setMonthlyProfit(monthlyProfit);
+
+    const yearlyProfit = dailyProfit * 365; // Assuming 365 days in a year
+    setYearlyProfit(yearlyProfit);
+  };
+
+
   const renderProfitCards = () => {
     return [
-      { title: 'Daily Profit', value: dailyProfit.toFixed(2) },
-      { title: 'Monthly Profit', value: monthlyProfit.toFixed(2) },
-      { title: 'Yearly Profit', value: yearlyProfit.toFixed(2) }
+      { title: 'Daily Profit', value: dailyProfit.toFixed(2), icon: 'money-bill-wave' },
+      { title: 'Monthly Profit', value: monthlyProfit.toFixed(2), icon: 'money-bill-wave' },
+      { title: 'Yearly Profit', value: yearlyProfit.toFixed(2), icon: 'money-bill-wave' }
     ].map((item, index) => (
       <Card key={index} style={styles.card}>
         <Card.Content>
+          <FontAwesome5 name={item.icon} size={24} color="#107c2e" style={styles.profitIcon} />
           <Text style={styles.cardTitle}>{item.title}</Text>
           <Text style={styles.cardText}>{item.value}</Text>
         </Card.Content>
@@ -82,76 +90,68 @@ const ProfitCalculatorScreen = () => {
   );
 
   return (
-    <ScrollView contentContainerStyle={styles.scrollContainer}>
-      <GestureHandlerRootView>
-        <View style={styles.container}>
-          <Text>Date:</Text>
-          <View style={styles.inputContainer}>
-            <Ionicons name="calendar-outline" size={24} color="black" style={styles.icon} />
-            <TextInput
-              style={styles.input}
-              value={date}
-              onChangeText={setDate}
-              keyboardType="datetime"
-            />
-          </View>
-          <Text>Price per Litre:</Text>
-          <View style={styles.inputContainer}>
-            <Ionicons name="cash-outline" size={24} color="black" style={styles.icon} />
-            <TextInput
-              style={styles.input}
-              value={price}
-              onChangeText={setPrice}
-              keyboardType="decimal-pad"
-            />
-          </View>
-          <Text>Quantity (Litres):</Text>
-          <View style={styles.inputContainer}>
-            <Ionicons name="cube-outline" size={24} color="black" style={styles.icon} />
-            <TextInput
-              style={styles.input}
-              value={quantity}
-              onChangeText={setQuantity}
-              keyboardType="numeric"
-            />
-          </View>
+    <GestureHandlerRootView style={styles.container}>
+      <Text>Date:</Text>
+      <View style={styles.inputContainer}>
+        <Ionicons name="calendar-outline" size={24} color="black" style={styles.icon} />
+        <TextInput
+          style={styles.input}
+          value={date}
+          onChangeText={setDate}
+          keyboardType="default"
+        />
+      </View>
+      <Text>Price per Litre:</Text>
+      <View style={styles.inputContainer}>
+        <Ionicons name="cash-outline" size={24} color="black" style={styles.icon} />
+        <TextInput
+          style={styles.input}
+          value={price}
+          onChangeText={setPrice}
+          keyboardType="numeric"
+        />
+      </View>
+      <Text>Quantity (Litres):</Text>
+      <View style={styles.inputContainer}>
+        <Ionicons name="cube-outline" size={24} color="black" style={styles.icon} />
+        <TextInput
+          style={styles.input}
+          value={quantity}
+          onChangeText={setQuantity}
+          keyboardType="numeric"
+        />
+      </View>
 
-          <TouchableOpacity style={styles.button} onPress={calculateProfit}>
-            <Text style={styles.buttonText}>Calculate Profit</Text>
-          </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={calculateProfit}>
+        <Text style={styles.buttonText}>Calculate</Text>
+      </TouchableOpacity>
 
-          
-
-          <View style={styles.profitContainer}>
-            {renderProfitCards()}
-          </View>
-          <TouchableOpacity style={styles.button} onPress={handleSubmit}>
-            <Text style={styles.buttonText}>Submit</Text>
-          </TouchableOpacity>
-          {tableData.length > 0 && (
-            <View style={styles.tableContainer}>
-
-              <Text style={styles.tableHeader}>Table Data</Text>
-              <FlatList
-                data={tableData}
-                renderItem={renderItem}
-                keyExtractor={item => item.id.toString()} // Assuming each item has an 'id' property
-              />
-            </View>
-          )}
+      <View style={styles.profitContainer}>
+        {renderProfitCards()}
+      </View>
+      <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
+      {error && <Text style={styles.errorText}>{error}</Text>}
+      {tableData.length > 0 && (
+        <View style={styles.tableContainer}>
+          <Text style={styles.tableHeader}>Table Data</Text>
+          <FlatList
+            data={tableData}
+            renderItem={renderItem}
+            keyExtractor={item => item.id.toString()}
+          />
         </View>
-      </GestureHandlerRootView>
-    </ScrollView>
+      )}
+    </GestureHandlerRootView>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollContainer: {
-    flexGrow: 1,
-  },
   container: {
+    flex: 1,
     padding: 20,
-    backgroundColor: '#FFFFFF', // Color.colorWhite
+    backgroundColor: '#FFFFFF',
   },
   inputContainer: {
     flexDirection: 'row',
@@ -169,28 +169,51 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   button: {
-    backgroundColor: '#107c2e', // Adjust button style to match Property1Default
-    padding: 10,
+    backgroundColor: '#107c2e',
+    padding: 8,
     alignItems: 'center',
     borderRadius: 5,
-    marginTop: 10, // Adjust spacing as needed
+    marginTop: 10,
   },
   buttonText: {
     color: '#f7b304',
     fontWeight: 'bold',
   },
-  profitText: {
-    // Style your profit text here to match the startYourJourney text
-    // Example: color: '#000000'
+  profitContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 20,
   },
-  // Table styles
+  card: {
+    flex: 1,
+    marginHorizontal: 5,
+    elevation: 4,
+  },
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  cardText: {
+    fontSize: 16,
+  },
+  submitButton: {
+    backgroundColor: '#107c2e',
+    padding: 8,
+    alignItems: 'center',
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  profitIcon: {
+    alignSelf: 'center',
+    marginBottom: 10,
+  },
   tableContainer: {
     marginTop: 20,
   },
   tableHeader: {
     fontSize: 16,
     fontWeight: 'bold',
-    // Additional styles to match the startYourJourney text
   },
   tableRow: {
     flexDirection: 'row',
@@ -204,24 +227,9 @@ const styles = StyleSheet.create({
     flex: 1,
     textAlign: 'center',
   },
-  // Card styles
-  profitContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 20,
-  },
-  card: {
-    flex: 1,
-    marginHorizontal: 5,
-    elevation: 4, // Add elevation for shadow on Android
-  },
-  cardTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 5,
-  },
-  cardText: {
-    fontSize: 16,
+  errorText: {
+    color: 'red',
+    marginTop: 10,
   },
 });
 
